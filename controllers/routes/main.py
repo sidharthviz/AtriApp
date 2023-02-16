65ba9c3b43e294521798f5fba204cf3a45002560
Siddharth verma <990.siddharthverma@gmail.com>
2023-02-16 21:08:04 +0530

    added file

--- controllers/routes/main.py
+++ controllers/routes/main.py
@@ -10,7 +10,7 @@ def init_state(at: Atri):
     """
     at.bar1.custom.data = [{"x":2010, "category1":40, "category2":50}, 
                            {"x":2011, "category1":30, "category2":60}]
-                           
+
     pass
 
 def handle_page_request(at: Atri, req: Request, res: Response, query: str):
